81c2ad52dfdf0f90992b3f0468c6880d32eb781d
Brian Salemi <briandsalemi@gmail.com>
2019-06-13 18:49:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,7 +41,7 @@ WHERE category == 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT projects.category, SUM(pledges.amount) FROM projects 
-INNER JOIN pledges ON project.id = pledges.project_id 
+"SELECT projects.category, SUM(pledges.amount) FROM projects
+INNER JOIN pledges ON project.id = pledges.project_id
 WHERE category == 'books'"
 end
